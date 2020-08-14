58f8b67db0c42357fa90189e2cb320b19ebf800d
Julia Guida <julia.guida@FT-MW4941.local>
2020-08-14 13:51:55 +0100

    This is my initial commit

---
+++ lesson1.py
@@ -0,0 +1,41 @@
+
+guess = input('Please guess a number between 1 and 10? ')
+
+guess = int(guess)
+if  guess == 8:
+    print('Well done you got it right ' 'Now lets play another games ?' )
+
+elif guess  > 8:
+
+    print('Please try again the number is too hight ' )
+
+elif guess < 8:
+    print('Keep trying this time the number was too low. ' )
+
+
+    
+guess = input('Please guess a number between 1 and 10 ' ) 
+
+     
+
+
+if int(guess) == 7:
+  print("You got it!!! Now lets play another games ?" )
+elif int(guess) < 7: 
+  print("You guessed too low ")
+elif int(guess)  > 7:
+  print("You guessed too high ") 
+
+apples =  int(input('How many apple did you buy ? ' ))
+oranges = int(input('How many oranges ? ' ))
+#total = apples + oranges,
+print('Total of fruits you bought today is' ' = ',apples + oranges )
+
+
+numbers = int(input('What is your favourite number? '))
+  
+  numbers = int(0
+
+  for i in numbers(3):
+    total = += 1
+    print(numbers)
