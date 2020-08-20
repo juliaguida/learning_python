70de962c9b207641ac855d53e99ea37c4f59b5bf
julia.guida <julia.guida@ft.com>
2020-08-20 17:18:19 +0100

    Write a program that takes 10 numbers as input from the user and print the sum of those 10 numbers.

--- homework1.py
+++ homework1.py
@@ -1,3 +1,13 @@
 # Write a program that takes 10 numbers as input from the user and print the sum of those 10 numbers.
 
-print('Hello user')
+
+number = []
+for i in range(10):
+    input_number = int(input('Please enter a number '))
+    number.append(input_number)
+
+total = 0
+for i in number:
+    total += i
+print('The total of number you entered is', total )
+
