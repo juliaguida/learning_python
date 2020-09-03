e195495f10e08817740b6c96e951d09d76d031b5
julia.guida <julia.guida@ft.com>
2020-09-03 12:28:58 +0100

    variable and while expression

--- homework2.py
+++ homework2.py
@@ -1,12 +1,19 @@
 # Practise if else, nested if else and while loops.
 
-x = 11
+"""x = 11
 if x > 10:
    
   print('x is greater than 10 ')
 
 elif x < 10:
-   print(' x is not greater than 10 ')
+   print(' x is not greater than 10 ')"""
+   
 
+#while loop:
+x = 11
+
+while x > 10:
+    
+  
 
 
