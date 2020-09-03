d6b486cb9e7e57b7a2adf2e9941b05776556d90b
julia.guida <julia.guida@ft.com>
2020-09-03 13:59:17 +0100

    while loop and if statement

--- homework2.py
+++ homework2.py
@@ -1,23 +1,25 @@
 # Practise if else, nested if else and while loops.
 
-"""x = 11
-if x > 10:
-   
-  print('x is greater than 10 ')
-
-elif x < 10:
-   print(' x is not greater than 10 ')"""
-   
-
-#while loop:
 x = 11
+if x > 10:
+   print('x is greater than 10 ')
+elif x < 10:
+   print(' x is not greater than 10 ')
+   
+#commets multiple lines command forward slash
+#while loop:
 
+x = 12
 while x > 10:
-    print( 'x is greater than 10 ')
-    break
+    print( '{} is greater than 10 '.format(x))
+    x = x-1 
+   
 if x == 10:
-    print(' x is not greater than 10')
+    print(' x is iqual to 10')
 else:
     print('loop out')
+
+
+
 
   
