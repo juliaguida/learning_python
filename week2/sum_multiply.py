18acc95cbfa97596dff7d94a6f3196e29f2f0d8f
julia.guida <julia.guida@ft.com>
2020-09-24 19:26:41 +0100

    adding and multiply two inputs number from the user

---
+++ week2/sum_multiply.py
@@ -0,0 +1,8 @@
+# Write a program that takes two inputs from the user and display the sum and multiplication result of the two numbers.
+
+numb_one = int(input(' Please enter a number: '))
+numb_two = int(input(' Please enter another number: '))
+total = numb_one + numb_two
+print(total)
+mult = numb_one * numb_two
+print(mult)
