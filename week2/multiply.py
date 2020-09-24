1fc15cf9646d07d54af40fd811f505a7ced30a9b
julia.guida <julia.guida@ft.com>
2020-09-24 19:16:18 +0100

    added code to multiply numbers

---
+++ week2/multiply.py
@@ -0,0 +1,21 @@
+# write a program to multiply 2 numbers and print the result of the operation.
+
+# Multiply 2 constant and print the result of the multiplication of those numbers.
+result = 3 * 1
+print(result)
+
+# Multiply two variable numbers.
+a = 3 
+b = 1
+result = a * b
+print(result)
+
+# Multiply two numbers inputed by user.
+
+a = int(input(' Please enter a number: '))
+b = int(input(' Please enter another number: '))
+result = a * b
+print(result)
+
+
+
