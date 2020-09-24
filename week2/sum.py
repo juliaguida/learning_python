1480464d8b4739366536eebf361646aee081b3c8
julia.guida <julia.guida@ft.com>
2020-09-24 18:58:15 +0100

    created a program to add two numbers

---
+++ week2/sum.py
@@ -0,0 +1,19 @@
+# adding two constant number.
+sum = 10 + 6
+print(sum)
+
+# adding two variable numbers.
+a = 10
+b = 6
+sum = a + b
+print(sum)
+
+# # Write a program that inputs 2 numbers 
+# from the user and print the sum of the numbers.
+
+a =  int(input('Please enter a number: '))
+b =  int(input('Please enter a number: '))
+sum = a + b
+print(sum)
+
+
