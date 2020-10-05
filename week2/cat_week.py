ad0b44278a2cb86b337f02b632102eb7e179dabf
julia.guida <julia.guida@ft.com>
2020-10-05 19:49:07 +0100

    Prints the number of cans a cat needs per day.

---
+++ week2/cat_week.py
@@ -0,0 +1,7 @@
+# prints the total number of can of food 10 cats needs per week.
+
+cats = 10
+can_each_cat = 2
+days_in_week = 7
+cat_can_weekly = cats * can_each_cat * days_in_week 
+print(cat_can_weekly)
---
+++ week2/cats_cans.py
@@ -0,0 +1,6 @@
+#Takes the number of cats and multiply by the can each cat.
+# This program prints the number of food cans a 10 cat needs per day.
+cat = 10
+can_each_cat = 2
+can_all_cats = cat * can_each_cat
+print(can_all_cats)
