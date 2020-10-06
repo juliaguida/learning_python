7f20fbfbf2a279ba2105fddef31e94bd75c8c699
julia.guida <julia.guida@ft.com>
2020-10-06 19:56:25 +0100

    variable cat

--- week2/cat_week.py
+++ week2/cat_week.py
@@ -1,7 +1,3 @@
 # prints the total number of cans 10 cats needs per week.
 
 cats = 10
-can_each_cat = 2
-days_in_week = 7
-cat_can_weekly = cats * can_each_cat * days_in_week 
-print(cat_can_weekly)
