57b8db635a4c4bae11ecc6cb8d035bbe6dc2fefc
julia.guida <julia.guida@ft.com>
2020-10-06 19:53:23 +0100

    .format use

---
+++ testcode.py
@@ -0,0 +1,60 @@
+# a = [ 'a', 'b', 'c' ]
+# while a:
+#     #if len(a) > 3:
+#       print(a.pop())
+#     print('done.')
+
+# a = ['foo', 'bar', 'baz', 'qux', 'corge']
+# while a:
+#     if len(a) < 3:
+#         break
+#     print(a.pop())
+# print('Done.')
+#         break
+
+# i = 0
+# number = int(input('Enter a number '))
+# answer = number %2
+# if answer == 0:
+#     print(number, 'this number is a even number ')
+# else:
+#     print('{} is not divided by 2 '.format(number))
+
+
+# total = 0
+# numb1 = int(input('Enter a number '))
+# numb2 = int(input('Enter a number '))
+# while i < 3:
+# if total / 2:
+#       total += 1
+#     print( total,'Numb1 is devide by numb2 ')
+ 
+# else:
+#     print( total, 'is not divided by 2 ')
+ 
+# birth_year = int(input('birth year '))
+# age = 2020 - birth_year
+# print(' You are {} year old'.format(age))
+
+# weight_lbs = input('Please enter your weight ' )
+# weight_kg = int(weight_lbs) * 45
+# print(weight_kg)
+
+# course = """ 
+
+# Hi John,
+
+# We are coming through this email to inform you about our new python course
+
+# Please click on the link to find out more.
+
+# Best wishes.
+
+# """
+# print(course)
+
+days = 31
+hours = 24
+total_hours = days * hours
+message = ‘There are {} hours in {} days’.format(total_hours,days)’
+     print(message)
