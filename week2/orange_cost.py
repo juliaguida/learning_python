0662e4f6c19426d9ed4f0a92ccb06297c08d78e1
julia.guida <julia.guida@ft.com>
2020-10-07 19:01:28 +0100

    user input

---
+++ week2/orange_cost.py
@@ -0,0 +1,4 @@
+# print the total cost of the oranges entered by the user
+
+cost_orange = 0.50
+user_input = int(input( 'How many oranges do you have ? Tell me and I will calculate the price for you. Enter in here:  '))
