d1e2f51ca911ea8accafae9466b78a74ffdd1c04
julia.guida <julia.guida@ft.com>
2020-10-08 17:54:29 +0100

    reminder and div of two numbers

--- week2/div_remind.py
+++ week2/div_remind.py
@@ -1,2 +1,6 @@
 # prints a division and reminder of two numbers
-numb1 =  int(input('Please enter a number: ?'))
+numb1 =  int(input('Please enter a number: '))
+numb2 = int(input('Please enter another number: '))
+div_result = numb1 / numb2
+rem_result = numb1 % numb2
+print('Div result is {} and the reminder is {}'.format(div_result,rem_result))
