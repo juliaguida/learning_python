27a42827ea8379ff3fcda1a2b9b88bfc00f89082
julia.guida <julia.guida@ft.com>
2020-10-08 19:02:47 +0100

    used title method on the user input name

--- week3/string_input.py
+++ week3/string_input.py
@@ -1,6 +1,7 @@
 
 name_user = input('What is your name? ')
 age_user = int(input('What is your age? '))
-print('Hi {} you are {} years old'.format(name_user,age_user))
+title_name_user = name_user.title()
+print('Hi {} you are {} years old'.format(title_name_user,age_user))
 
 
