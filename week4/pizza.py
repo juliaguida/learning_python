06dd7428a150532231d0614d8cb6a8491ce2ef87
julia.guida <julia.guida@ft.com>
2020-10-20 08:36:45 +0100

    takes a input of the user and calculate the number of pizzas needed.

---
+++ week4/pizza.py
@@ -0,0 +1,7 @@
+#This code takes the number of pizzas needed to feed 6 people.
+
+numb_of_people =  int(input("Let's to order some pizza. How many of us ? "))
+numb_pizzas = 0.5
+pizza_order = numb_of_people * numb_pizzas
+print('We need {} pizzas for all of us.'.format(pizza_order))
+
