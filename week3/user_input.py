9ff0f212208f3cf1e0c7f6b3299f5db383f99637
julia.guida <julia.guida@ft.com>
2020-10-20 09:26:40 +0100

    return if the user imput is greater than 10

---
+++ week3/user_input.py
@@ -0,0 +1,10 @@
+#Take an input from the user and check if it's greater than 10
+
+user_input = int(input('Please enter a number: '))
+message = user_input
+if message > 10:
+    print( 'Input was greater than 10')
+else:
+    print( 'Input is not greater than 10')
+
+
