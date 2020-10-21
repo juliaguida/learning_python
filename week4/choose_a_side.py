ae53db1825100f984e28045883597ab7190873dd
julia.guida <julia.guida@ft.com>
2020-10-21 08:23:20 +0100

    Body of the function with the code logic.

--- week4/choose_a_side.py
+++ week4/choose_a_side.py
@@ -1,4 +1,11 @@
 import turtle
 
 def shape(side_lenght, angle,number_of_sides):
-    
+    turtle.begin_fill()
+    turtle.fillcolor('black')
+    for i in range(9):
+        turtle.forward(side_length)
+        turtle.right(number_of_sides)
+    turtle.end_fill()
+   
+ 
