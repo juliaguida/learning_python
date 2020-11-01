dfa93279b8bb4d15226bc4d504e16ae039b873d2
julia.guida <julia.guida@ft.com>
2020-11-01 15:52:30 +0000

    check if the user input is equsl to the string "I am allowed

--- week6/check_correct_pass.py
+++ week6/check_correct_pass.py
@@ -1,6 +1,6 @@
 Password = 'I am allowed'
 user_input = input('Please enter your password: ')
-# if user_input == 'I am allowed':
+if user_input == 'I am allowed':
 #     print('This is the correct password')
 # else:
 #     print('This password is wrong')
