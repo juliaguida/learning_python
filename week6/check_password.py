0776cff9653f806c8c42fd9bf55b1437ed765da7
julia.guida <julia.guida@ft.com>
2020-11-01 15:39:22 +0000

    This code check if the password is correct

---
+++ week6/check_password.py
@@ -0,0 +1,9 @@
+# Write a program to check if you are the admin and if you have entered the right password, reply with ‘Welcome’ or ‘Go Away’ message
+
+
+password = 'I am allowed'
+user_input = input('Please enter you password: ')
+if user_input == 'I am allowed':
+    print('Welcome!!!')
+else:
+    print('Go Away!!!')
