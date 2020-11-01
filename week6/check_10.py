0e5667a506f8ff392ad0aae783f918e8cba5c0a4
julia.guida <julia.guida@ft.com>
2020-11-01 15:12:46 +0000

    check if 10 is greater tha the user input

--- week6/check_10.py
+++ week6/check_10.py
@@ -1,6 +1,6 @@
 #Write a program that takes an input from the user and prints “Input was greater than 10” if the number was greater than 10
 check_numb = int(input('Please enter a number: '))
-# if check_numb > 10:
+ if check_numb > 10:
 #     print('The number {} is greater than 10 '.format(check_numb))
 # elif check_numb < 10:
 #     print('This number is not greater than 10'.format(check_numb))
