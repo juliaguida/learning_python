4698e2be9d21cb4a91d922fc36cb4fb19c7a43ad
julia.guida <julia.guida@ft.com>
2020-11-01 15:51:25 +0000

    takes the user input

--- week6/check_correct_pass.py
+++ week6/check_correct_pass.py
@@ -1,5 +1,5 @@
 Password = 'I am allowed'
-# user_input = input('Please enter your password: ')
+user_input = input('Please enter your password: ')
 # if user_input == 'I am allowed':
 #     print('This is the correct password')
 # else:
