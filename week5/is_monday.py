15bce4ae9305077d43516bcfa62f716703897e24
julia.guida <julia.guida@ft.com>
2020-11-10 16:58:53 +0000

    This program takes 2 numbers as arguments to a function and returns the sum of those 2 numbers.

---
+++ week5/is_monday.py
@@ -0,0 +1,11 @@
+#Get the today from the user
+#Compare today == Monday
+#Print today is Monday: True or False
+
+
+
+
+today = input('What is day it? ')
+is_monday = today.title() == 'Monday'
+print('Today is Monday {}'.format(is_monday))
+
