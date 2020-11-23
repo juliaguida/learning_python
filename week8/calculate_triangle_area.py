9f4af50900aa181c17b5fef2e457a9cc60ebd448
julia.guida <julia.guida@ft.com>
2020-11-23 10:33:14 +0000

    takes an input from the user and calculates the area of the triangle

--- week8/calculate_triangle_area.py
+++ week8/calculate_triangle_area.py
@@ -1,11 +1,3 @@
-# def triangle(b, h):
-#     area = b * h /2
-#     return area
-# base = int(input('Enter the base of a triangle please: '))
-# height = int(input('Enter the base of a triangle please: '))
-# print(triangle(base,height))
-
-  
 def triangle_2(): #called function
     base = int(input('Enter the base of a triangle please: '))
     height = int(input('Enter the height of a triangle please: '))
@@ -15,13 +7,6 @@ def triangle_2(): #called function
 print('Hello')
 triangle_2() #calling function
 print('bye')
-
-# b = int(input("Input the base : "))
-# h = int(input("Input the height : "))
-
-# area = b*h/2
-
-# print("area = {} ".format(area))
 
 
 
