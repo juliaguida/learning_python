4f47f9b67dfca081c110e3d321ab0fd0151b7efb
julia.guida <julia.guida@ft.com>
2020-11-23 10:41:30 +0000

    ask the user to enter a side for the for the triangle

--- week4/choose_a_side.py
+++ week4/choose_a_side.py
@@ -1,11 +1,13 @@
 import turtle
 
-def shape(side_lenght, angle,number_of_sides):
-    turtle.begin_fill()
-    turtle.fillcolor('black')
-    for i in range(9):
+def shape(number_of_sides,side_length):
+    angle = 360/number_of_sides   
+    for i in range(number_of_sides):
         turtle.forward(side_length)
-        turtle.right(number_of_sides)
-    turtle.end_fill()
-   
- 
+        turtle.right(angle)
+    turtle.done()
+          
+
+number_of_sides = int(input('Please input the numbers of side: '))
+side_length = int(input('Please input the of side length: '))
+shape(number_of_sides,side_length)
