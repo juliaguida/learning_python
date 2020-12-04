4cccc1d3014ec8b98166bc17c45a80a89c5608bb
julia.guida <julia.guida@ft.com>
2020-12-04 08:05:48 +0000

    This code checks if a number is positive or negative

---
+++ week8/check_numb.py
@@ -0,0 +1,6 @@
+# This code checks if a number is positive or negative
+number = float(input('Please enter a number to check if it is negative or positive.'))
+if number > 0:
+    print('The input is positive')
+else:
+    print( 'This input is negative')
