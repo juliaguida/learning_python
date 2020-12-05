330710c976d43bcc9dcef3e590925ed11ec5e6e9
julia.guida <julia.guida@ft.com>
2020-12-05 12:11:10 +0000

    Takes an input from the user and reverse it and print the length of it.

---
+++ week10/print_reverse.py
@@ -0,0 +1,10 @@
+# Write a program to take a sentence from the user and 
+# Print the length of the sentence,
+# Print the sentence in reverse order and
+
+phrase = input( 'Please enter any phrase that comes in your mind.')
+reversedstring=''.join(reversed(phrase))
+print(reversedstring)
+print(len(reversedstring))
+
+
