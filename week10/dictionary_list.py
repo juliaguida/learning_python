d1a95a8958af5822647d44f9dda03340bca6659b
julia.guida <julia.guida@ft.com>
2021-01-27 21:47:58 +0000

    Dictionay list

---
+++ week10/dictionary_list.py
@@ -0,0 +1,12 @@
+people = [
+    {'name': 'Jessica', 'age': 23},
+    {'name': 'Trisha', 'age':25}
+]
+
+for person in people:
+    print(person)
+    print(person['name'],person['age'])
+
+
+
+    
